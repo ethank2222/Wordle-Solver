b7f7ecd594b94639a34089a79bf9f7bbb234eff8
Jeshwin Prince <jeshwinjprince@gmail.com>
2025-05-15 22:20:00 -0700

    Fix JSON loading bug

--- scripts/inject.js
+++ scripts/inject.js
@@ -42,121 +42,103 @@ for (var i = 0; i < 5; i++) {
     container.appendChild(paragraph);
 }
 
+async function loadJSON(fileName) {
+    return fetch(chrome.runtime.getURL(fileName)).then((resp) => resp.json());
+}
+
 // Get list of possible answers from data/wordle.json
-async function loadJSON() {
-    try {
-        const response = await fetch(chrome.runtime.getURL("data/wordle.json"));
-        const jsonData = await response.json();
-        console.log("JSON data loaded:", jsonData);
-        return jsonData;
-    } catch (error) {
-        console.error("Error loading JSON:", error);
-    }
-}
+loadJSON("data/wordle.json").then((choices) => {
+    var numGuesses = 0;
+    var letters = [];
 
-var choices;
-
-loadJSON()
-    .then((data) => {
-        choices = data;
-        console.log("Wordle data is now available", data);
-    })
-    .catch((error) => {
-        console.error("Failed to load wordle data:", error);
-    });
-
-var numGuesses = 0;
-var letters = [];
-
-function autofill() {
-    var next = choices[0];
-    var score = 0;
-    for (var i = 0; i < choices.length; i++) {
-        var mycount = 0;
-        for (var j = 0; j < 5; j++) {
-            mycount += dictionary[choices[i].substring(j, j + 1).toUpperCase()];
-            for (k = j; k < 5; k++) {
-                if (choices[i].substring(j, j + 1).toLowerCase() == choices[i].substring(k, k + 1).toLowerCase()) {
-                    mycount -= 5;
-                }
-            }
-        }
-        if (mycount > score) {
-            next = choices[i];
-            score = mycount;
-        }
-    }
-    if (numGuesses == 0) {
-        next = "salet";
-    }
-    document.querySelectorAll("[data-key='" + next.substring(0, 1) + "']")[0].click();
-    document.querySelectorAll("[data-key='" + next.substring(1, 2) + "']")[0].click();
-    document.querySelectorAll("[data-key='" + next.substring(2, 3) + "']")[0].click();
-    document.querySelectorAll("[data-key='" + next.substring(3, 4) + "']")[0].click();
-    document.querySelectorAll("[data-key='" + next.substring(4, 5) + "']")[0].click();
-    document.querySelectorAll("[data-key='↵']")[0].click();
-    numGuesses++;
-    setTimeout(guess, 4000);
-}
-
-function guess() {
-    if (numGuesses == 6 || choices.length == 1) {
-        return;
-    }
-    for (var i = 0; i < 5; i++) {
-        var row = document.getElementsByClassName("Row-module_row__pwpBq")[numGuesses - 1];
-        var iter = row.childNodes[i].firstChild;
-        if (iter.getAttribute("data-state") == "absent") {
-            var count = 0;
-            for (var k = 0; k < 5; k++) {
-                if (iter.innerText.toLowerCase() == row.childNodes[k].firstChild.innerText.toLowerCase()) {
-                    count++;
-                }
-            }
-            for (var k = 0; k < letters.length; k++) {
-                if (letters[k] == iter.innerText.toLowerCase()) {
-                    count--;
-                }
-            }
-            for (var j = 0; j < choices.length; j++) {
-                var count2 = 0;
-                for (var k = 0; k < 5; k++) {
-                    if (iter.innerText.toLowerCase() == choices[j].substring(k, k + 1)) {
-                        count2++;
+    function autofill() {
+        var next = choices[0];
+        var score = 0;
+        for (var i = 0; i < choices.length; i++) {
+            var mycount = 0;
+            for (var j = 0; j < 5; j++) {
+                mycount += dictionary[choices[i].substring(j, j + 1).toUpperCase()];
+                for (k = j; k < 5; k++) {
+                    if (choices[i].substring(j, j + 1).toLowerCase() == choices[i].substring(k, k + 1).toLowerCase()) {
+                        mycount -= 5;
                     }
                 }
-                if (count2 >= count || choices[j].substring(i, i + 1) == iter.innerText.toLowerCase()) {
-                    choices.splice(j, 1);
-                    j--;
-                }
             }
-            letters.push(iter.innerText.toLowerCase());
-        } else if (iter.getAttribute("data-state") == "present") {
-            for (var j = 0; j < choices.length; j++) {
-                if (
-                    choices[j].indexOf(iter.innerText.toLowerCase()) == -1 ||
-                    choices[j].substring(i, i + 1) == iter.innerText.toLowerCase()
-                ) {
-                    choices.splice(j, 1);
-                    j--;
-                }
+            if (mycount > score) {
+                next = choices[i];
+                score = mycount;
             }
-        } else if (iter.getAttribute("data-state") == "correct") {
-            for (var j = 0; j < choices.length; j++) {
-                if (choices[j].substring(i, i + 1) != iter.innerText.toLowerCase()) {
-                    choices.splice(j, 1);
-                    j--;
+        }
+        if (numGuesses == 0) {
+            next = "salet";
+        }
+        document.querySelectorAll("[data-key='" + next.substring(0, 1) + "']")[0].click();
+        document.querySelectorAll("[data-key='" + next.substring(1, 2) + "']")[0].click();
+        document.querySelectorAll("[data-key='" + next.substring(2, 3) + "']")[0].click();
+        document.querySelectorAll("[data-key='" + next.substring(3, 4) + "']")[0].click();
+        document.querySelectorAll("[data-key='" + next.substring(4, 5) + "']")[0].click();
+        document.querySelectorAll("[data-key='↵']")[0].click();
+        numGuesses++;
+        if (numGuesses < 6) setTimeout(guess, 4000);
+    }
+
+    function guess() {
+        if (choices.length == 1) return;
+        for (var i = 0; i < 5; i++) {
+            var row = document.getElementsByClassName("Row-module_row__pwpBq")[numGuesses - 1];
+            var iter = row.childNodes[i].firstChild;
+            if (iter.getAttribute("data-state") == "absent") {
+                var count = 0;
+                for (var k = 0; k < 5; k++) {
+                    if (iter.innerText.toLowerCase() == row.childNodes[k].firstChild.innerText.toLowerCase()) {
+                        count++;
+                    }
+                }
+                for (var k = 0; k < letters.length; k++) {
+                    if (letters[k] == iter.innerText.toLowerCase()) {
+                        count--;
+                    }
+                }
+                for (var j = 0; j < choices.length; j++) {
+                    var count2 = 0;
+                    for (var k = 0; k < 5; k++) {
+                        if (iter.innerText.toLowerCase() == choices[j].substring(k, k + 1)) {
+                            count2++;
+                        }
+                    }
+                    if (count2 >= count || choices[j].substring(i, i + 1) == iter.innerText.toLowerCase()) {
+                        choices.splice(j, 1);
+                        j--;
+                    }
+                }
+                letters.push(iter.innerText.toLowerCase());
+            } else if (iter.getAttribute("data-state") == "present") {
+                for (var j = 0; j < choices.length; j++) {
+                    if (
+                        choices[j].indexOf(iter.innerText.toLowerCase()) == -1 ||
+                        choices[j].substring(i, i + 1) == iter.innerText.toLowerCase()
+                    ) {
+                        choices.splice(j, 1);
+                        j--;
+                    }
+                }
+            } else if (iter.getAttribute("data-state") == "correct") {
+                for (var j = 0; j < choices.length; j++) {
+                    if (choices[j].substring(i, i + 1) != iter.innerText.toLowerCase()) {
+                        choices.splice(j, 1);
+                        j--;
+                    }
                 }
             }
         }
+        document.getElementsByClassName("more-info")[numGuesses - 1].innerText =
+            numGuesses + ") " + choices.length + " possibilities left, ";
+
+        autofill();
     }
-    document.getElementsByClassName("more-info")[numGuesses - 1].innerText =
-        numGuesses + ") " + choices.length + " possibilities left, ";
 
     autofill();
-}
-
-autofill();
+});
 
 /*
 1. Add word to array if not in word
